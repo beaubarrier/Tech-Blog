441aa6464f8e1b832fddcef6b43edcc2cb11b48e
Beau Barrier <beaubarrier@gmail.com>
2021-08-09 09:31:53 -0700

    Update routes syntax

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const userRoutes = require('./userRoutes');
-const noteRoutes = require('./noteRoutes');
+const userRoutes = require('/userRoutes');
+const noteRoutes = require('/noteRoutes');
 
 router.use('/users', userRoutes);
 router.use('/notes', noteRoutes);
